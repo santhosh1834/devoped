9fa7986f57125540a645609f2dc82c9bfeb321be
santhosh1834 <santhosh.singari09@gmail.com>
2019-01-06 12:54:10 +0530

    dev

---
+++ src/main/java/devoped/App.java
@@ -0,0 +1,13 @@
+package devoped;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
